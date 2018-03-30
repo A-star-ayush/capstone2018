041d3ed12eb3f26fda402662903341f19de592b9
A-star-ayush <myselfthebest@yahoo.com>
2018-03-31 03:34:07 +0530

    refactored the code

--- ui/www/src/index.js
+++ ui/www/src/index.js
@@ -1,143 +1,131 @@
 // TODO : passwd should not be displayed while typing
 
-const https_address = "https://127.0.0.1:10000/";
+const serverAddress = "https://13.126.97.148:10000/";
+const geocodeAddress = "https://maps.googleapis.com/maps/api/geocode/";
+const jsonHook = "json?";
+const userHook = "usr?";
+const gpsHook = "gps?";
+const wisdomHook = "wisdom?";
 
 window.onload = function() {
-	var gpsMenu = document.getElementById("gpsMenu");
 	var gpsForm = document.getElementById("gpsForm");
-	var googleMap = document.getElementById("googleMap");
-
-	var userMenu = document.getElementById("userMenu");
 	var userForm = document.getElementById("userForm");
+	var analyticForm = document.getElementById("analyticForm");
 
-	userMenu.onclick = function() {
-		if (gpsForm.style.display == "block")
-			gpsForm.style.display = "";
-
-		if (userForm.style.display == "") {
-			userForm.style.display = "block";
-		}
-		else {
-			userForm.style.display = "";
-			userForm.name.value = "";
-			userForm.pass.value = "";
-		}
-
-		if (googleMap.style.display != "")
-			googleMap.style.display = "";
-	}
-
-	gpsMenu.onclick = function() {
-		if (userForm.style.display == "block")
-			userForm.style.display = "";
-		
-		if (gpsForm.style.display == "") {
-			gpsForm.style.display = "block";
-		}
-		else {
-			gpsForm.style.display = "";
-			gpsForm.time.value = "";
-			gpsForm.source.value = "";
-		}
-
-		if (googleMap.style.display != "")
-			googleMap.style.display = "";
-	}
+	var googleMap = document.getElementById("googleMap");
+	var geocodeOutput = document.getElementById("geocodeOutput");
+	var analyticOutput = document.getElementById("analyticOutput");
 
 	var session = 0;
-	var bl = require('bl');
 	var https = require('https');
 
+	// to allow node to connect through https to websites with self-signed certificates
 	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-	function makeHTTPSRequest(source, time, display, arr) {
-		https.get(https_address + "gps?source="+source+"&time="+time+"&session="+session, function(res) {
+	function myMap(lat, lng) {
+		var mapProp = {
+    		center:new google.maps.LatLng(lat, lng),
+    		zoom:15,
+		};
+			
+		var map = new google.maps.Map(googleMap, mapProp);
+	
+		var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lng),
+          map: map
+        });
+        googleMap.style.display = "block";
+	}
+
+	function makeHTTPSRequest(https_address, hook, req, callback) {
+		https.get(https_address + hook + req, function(res) {
 			res.setEncoding('utf8');
 			var result = "";
 			res.on('data', function(data) {
 				result = result + data;
 			});
 			res.on('end', function() {
-				if (res.statusCode != 200) {
-					googleMap.style.display = "";
-					console.log(res.statusCode);
-					console.log(result);
-					return;
-				}
-				var data = JSON.parse(result);
-				data = data[0];
-				console.log(data);
-				if (display) {
-					googleMap.style.display = "block";
-					myMap(data.lat, data.lng);
-					
-					/*https.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + data.lat+","+data.lng+
-					"&key=AIzaSyAj3V84hCmsgzxp_x_YESVNY1ttjgsoFY4", function(res2) {
-						res2.setEncoding('utf-8');
-						var result2 = "";
-						console.log("CHECK1");
-						res2.on('data', function(data){
-							console.log("CHECK2");
-							result2 += data;
-						});
-						res2.on('end', function() {
-							console.log("CHECK3");
-							var data2 = JSON.parse(result2);
-							console.log(data2['results'][0]['formatted_address']);
-						});
-					}); */
-							
-				} else {
-					arr.push(data);
-				}
+				var data = null;
+				if (res.statusCode == 200)
+					data = JSON.parse(result);
+				callback(res.statusCode, data);
 			});
 		}); 
-	}
-
-	gpsForm.onsubmit = function(e) {
-		e.preventDefault();
-		var time = gpsForm.time.value;
-		var source = gpsForm.source.value;
-
-		makeHTTPSRequest(source, time, true);
-	};
-
-
-	function myMap(lat, lng) {
-		var mapProp= {
-    		center:new google.maps.LatLng(lat, lng),
-    		zoom:15,
-		};
-			
-		var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	
-		var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(lat, lng),
-          map: map
-        });
 	}
 
 	userForm.onsubmit = function(e) {
 		e.preventDefault();
 		var name = userForm.name.value;
 		var pass = userForm.pass.value;
-
-		https.get(https_address + "usr?name="+name+"&pass="+pass, function(res) {
-			res.setEncoding('utf8');
-			var result = "";
-			res.on('data', function(data) {
-				result = result + data;
-			});
-			res.on('end', function() {
-				if (res.statusCode != 200) {
-					console.log(res.statusCode);
-					console.log(result);
-					return;
-				}
-				var data = JSON.parse(result);
-				session = data.session;
-				console.log(session);
-			});
-		}); 
+		var request = "name=" + name + "&pass=" + pass;
+		makeHTTPSRequest(serverAddress, userHook, request, userCallback);
 	};
+
+	gpsForm.onsubmit = function(e) {
+		e.preventDefault();
+		var time = gpsForm.time.value;
+		var source = gpsForm.source.value;
+		var request = "time=" + time + "&source=" + source + "&session=" + session;
+		makeHTTPSRequest(serverAddress, gpsHook, request, gpsCallback);
+	};
+
+	analyticForm.onsubmit = function(e) {
+		e.preventDefault();
+		var timeFrom = analyticForm.timeFrom.value;
+		var time = analyticForm.time.value;
+		var source = analyticForm.source.value;
+		var intervals = analyticForm.intervals.value;
+
+		var request = "source=" + source;	
+		if (timeFrom.length > 0)
+			request += "&timeFrom=" + timeFrom;
+		if (time.length > 0)
+			request += "&time=" + time;
+		if (intervals.length > 0)
+			request += "&intervals=" +intervals;
+		request += "&session=" + session;
+
+		makeHTTPSRequest(serverAddress, wisdomHook, request, analyticCallback);
+	};
+
+
+	function gpsCallback(statusCode, data) {
+		if (statusCode != 200) {
+			googleMap.style.display = "";
+			geocodeOutput.innerHTML = "Invalid server request";
+		}
+		else {
+			var first = data[0];
+			myMap(first.lat, first.lng);
+			var request = "latlng=" + first.lat +"," + first.lng + "&key=AIzaSyAj3V84hCmsgzxp_x_YESVNY1ttjgsoFY4";
+			geocodeOutput.innerHTML = "";
+			makeHTTPSRequest(geocodeAddress, jsonHook, request, geocodeCallback);
+		}
+	}
+
+	function geocodeCallback(statusCode, data) {
+		if (statusCode != 200)
+			geocodeOutput.innerHTML = "Invalid geocode request";
+		else 
+			geocodeOutput.innerHTML += data['results'][0]['formatted_address'] + "<br>";
+	}
+
+	
+	function analyticCallback(statusCode, data) {
+		if (statusCode != 200)
+			analyticOutput.innerHTML = "Invalid request";
+		else {
+			analyticOutput.innerHTML = JSON.stringify(data);
+		}
+	}
+
+	
+	function userCallback(statusCode, data) {
+		if (statusCode != 200)
+			geocodeOutput.innerHTML = analyticOutput.innerHTML = "Not logged in.";
+		else {
+			session = data.session;
+			geocodeOutput.innerHTML = analyticOutput.innerHTML = "Logged in.";
+		}			
+	}
 }
